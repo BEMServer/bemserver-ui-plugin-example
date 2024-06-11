f7aee394253a27b5c17c43ca374a27a8cfd2bb1f
David FREDERIQUE <dfrederique@nobatek.inef4.com>
2024-06-11 19:10:20 +0200

    Upgrade required UI version (>=0.7.0, <0.8.0)

--- bemserver_ui_plugin_example/plugin.py
+++ bemserver_ui_plugin_example/plugin.py
@@ -17,8 +17,8 @@ PLUGIN_INFO = {
 
 # min <= UI_VERSION < max
 REQUIRED_UI_VERSION = {
-    "min": Version("0.5.0"),
-    "max": Version("0.7.0"),
+    "min": Version("0.7.0"),
+    "max": Version("0.8.0"),
 }
 
 
